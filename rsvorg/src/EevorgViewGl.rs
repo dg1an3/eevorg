1be598f1541033588c1986c800c97feb4744b5eb
dg1an3 <dg1an3@github.com>
2025-01-09 09:50:35 -0600

    testing a rust implementation

--- rsvorg/src/EevorgViewGl.rs
+++ rsvorg/src/EevorgViewGl.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
 
 use windows::Win32::Foundation::*;
+use windows::Win32::Graphics::Gdi::*;
+use windows::Win32::Graphics::OpenGL::*;
+use windows::Win32::UI::WindowsAndMessaging::*;
+
 
 use crate::Eevorg::*;
 use crate::EevorgRenderer::*;
@@ -8,8 +12,8 @@ use crate::EevorgRenderer::*;
 pub struct CEevorgViewGl {
     m_oldRect: RECT,
     m_fRadius: f32,
-    // m_pDC: HDC,
-    // m_mapTextures: HashMap<Eevorg, GLuint>,
+    m_pDC: HDC,
+    m_mapTextures: HashMap<Eevorg, GLuint>,
     // the renderer for performing the drawing
     _renderer: EevorgRenderer,
 }
